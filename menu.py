d958991978fd71cf9991045750fb458de8389ef5
mohammad-mahdi <mohammadmahdi13831388@gmail.com>
2025-11-06 05:58:15 -1000

    abstract method , getter , setter , docstring , advanced structures control, start exceptin

--- menu.py
+++ menu.py
@@ -1,5 +1,11 @@
-menu_item = ["bergur" , "pizza" , "pasta" , "kfc"]
-price = [3.99,4.99,5.99,2.99]
+# menu_item = ["bergur" , "pizza" , "pasta" , "kfc"]
+# price = [3.99,4.99,5.99,2.99]
 
-for item,price in zip(menu_item,price):
-    print (f"{item:<10}  {price:>6.2f}$")
+# for item,price in zip(menu_item,price):
+#     print (f"{item:<10}  {price:>6.2f}$")
+
+
+dict = {"ali": "f566", "reza": "f4845", "javad": "g656560"}
+
+n_dict = {name: value[:2] for name, value in dict.items()}
+print(n_dict)
--- oop2.py
+++ oop2.py
@@ -1,20 +1,41 @@
+from abc import abstractmethod
+
 from cryptocode import decrypt, encrypt
 
 
-class Person:
+class person:
+
+    @abstractmethod
+    def get_total_person(self):
+        pass
+
+    @abstractmethod
+    def register(self, email, password):
+        pass
+
+    @abstractmethod
+    def __decrypt_password(self):
+        pass
+
+    @abstractmethod
+    def change_password(self, oldpassword, newpassword, newpassword2):
+        pass
+
+
+class Student(person):
 
     total_person = 0
 
     def __init__(self, first_name, last_name, age, country):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.__age = age
         self.country = country
-        Person.total_person += 1
+        Student.total_person += 1
 
     @staticmethod
     def get_total_person():
-        return Person.total_person
+        return Student.total_person
 
     def register(self, email, password):
         self.email = email
@@ -36,13 +57,22 @@ class Person:
         else:
             print("error password")
 
+    @property
+    def age(self):
+        return self.__age
 
-p1 = Person("mohammad mahdi", "abedinzadeh", 22, "iran")
-p1.rigister("mohammadmahdi13831388@gmail.com", "09906419973")
+    @age.setter
+    def age(self, value):
+        self.__age = value
+
+
+p1 = Student("mohammad mahdi", "abedinzadeh", 22, "iran")
+p1.register("mohammadmahdi13831388@gmail.com", "09906419973")
 print(p1.password)
 # print(p1.decrypt_password())
 p1.change_password(
     oldpassword="09906419973", newpassword="099064199733", newpassword2="099064199733"
 )
 # print(p1.decrypt_password())
-print(Person.get_total_person())
+print(Student.get_total_person())
+print(p1.age)
